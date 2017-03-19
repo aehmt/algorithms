e4a84abfb20d1cabff762035ba35766c95fc6939
aehmt <calisahmet@gmail.com>
2017-03-19 18:34:14 -0400

    prediction.py

---
+++ python/prediction-pyhton.py
@@ -0,0 +1,13 @@
+from sklearn import tree
+
+#[height, weight, shoe size]
+X = [[181, 80, 44], [177, 70 ,43], [160, 60, 38], [154, 54, 37], [166, 65, 40], [190, 90, 47], [175, 64, 39], [177, 70, 40], [159 , 55, 37]]
+Y = ['male', 'female', 'female', 'female', 'male', 'male', 'male', 'female', 'male']
+
+clf = tree.DecisionTreeClassifier()
+
+clf = clf.fit(X, Y)
+
+predicition = clf.predict([[190, 70, 43]])
+
+print predicition
