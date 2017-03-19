2b87916d31859527a3ced27224755c008bb0c147
aehmt <calisahmet@gmail.com>
2017-03-19 18:31:33 -0400

    linked-list.js

---
+++ js/linkedList.js
@@ -0,0 +1,11 @@
+function LinkedList(value) {
+  this.value = value;
+  this.next = null;
+}
+
+
+function DoublyLinkedList(value) {
+  this.value = value;
+  this.next = null;
+  this.previous = null;
+}
